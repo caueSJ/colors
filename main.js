ec61887fd338547eac6437310dd6373118f0a572
Caue SJ <caue.silvaa@gmail.com>
2023-09-23 19:38:55 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// const block = document.createElement('div');
+const main = document.querySelector('.main');
+const button = document.querySelector('.btn-change-color');
+const versionList = document.querySelector('#versionList');
+const changeBackgroundButton = document.querySelector('#changeBackgroundButton');
+const backButton = document.querySelector('#backButton');
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 255);
+}
+
+const getRandomColorRGB = () => {
+  return `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+}
+
+const changeColor = () => {  
+  main.style.backgroundColor = getRandomColorRGB();
+  button.style.backgroundColor = getRandomColorRGB();
+  button.style.color = button.style.borderColor = getRandomColorRGB();
+}
+
+const show = (...elements) => {
+  elements.forEach(element => {
+    element.classList.remove('hide');
+  });
+}
+
+const hide = (...elements) => {
+  elements.forEach(element => {
+    element.classList.add('hide');
+  });
+}
+
+const updateView = (version) => {
+  if (version > 0) {
+    hide(versionList);
+    show(changeBackgroundButton, backButton);
+  } else {
+    hide(changeBackgroundButton, backButton);
+    show(versionList);
+  }
+}
