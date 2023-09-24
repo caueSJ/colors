6d8a3ad1a05beb170e2c9d09576194a783944a92
Caue SJ <caue.silvaa@gmail.com>
2023-09-23 23:57:15 -0300

    - Doc some functions - Create gradient version - Split code

--- main.js
+++ main.js
@@ -5,18 +5,63 @@ const versionList = document.querySelector('#versionList');
 const changeBackgroundButton = document.querySelector('#changeBackgroundButton');
 const backButton = document.querySelector('#backButton');
 
-const getRandomNumber = () => {
-  return Math.floor(Math.random() * 255);
+/**
+ * Generates and returns a random number between 0 and maxValue.
+ * @param {Number} maxValue Maximum random value that can be "drawn".
+ * @returns {Number}
+ */
+const getRandomNumber = (maxValue) => {
+  return Math.floor(Math.random() * maxValue);
+}
+
+/**
+ * Get a random number between 0 and 255 (maximum allowed for RGB colors).
+ * @returns {Number} Returns a random number to be used as value for R, G and B.
+ */
+const getRandomRGBValue = () => {
+  return getRandomNumber(255);
+}
+
+/**
+ * Get a random value between "0deg" and "360deg".
+ * @returns {Number} Returns a random number to be used as a degree angle for a linear-gradient color.
+ */
+const getRandomAngle = () => {
+  return `${getRandomNumber(360)}deg`;
 }
 
 const getRandomColorRGB = () => {
-  return `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`;
+  return `rgb(${getRandomRGBValue()}, ${getRandomRGBValue()}, ${getRandomRGBValue()})`;
 }
 
-const changeColor = () => {
-  main.style.backgroundColor = getRandomColorRGB();
-  button.style.backgroundColor = getRandomColorRGB();
-  button.style.color = button.style.borderColor = getRandomColorRGB();
+/**
+ * Creates a random two-colors gradient with a random angle.
+ * @returns {String} Returns an linear-gradient string to be used as the background.
+ */
+const getRandomGradientColor = () => {
+  return `linear-gradient(${getRandomAngle()}, ${getRandomColorRGB()}, ${getRandomColorRGB()})`;
+}
+
+/**
+ * Change some colors.
+ *
+ * Version 1: Solid colors (only one color).
+ * Version 2: Gradient colors (two-colors gradients).
+ */
+const changeColor = (version) => {
+  switch (version) {
+    case 1:
+      main.style.backgroundColor = getRandomColorRGB();
+      button.style.backgroundColor = getRandomColorRGB();
+      button.style.color = button.style.borderColor = getRandomColorRGB();
+      break;
+    case 2:
+      main.style.background = getRandomGradientColor();
+      button.style.background = getRandomGradientColor();
+      break;
+    default:
+      break;
+  }
 }
 
 const show = (...elements) => {
@@ -36,13 +81,20 @@ const resetColors = () => {
   button.removeAttribute('style');
 }
 
-const updateView = (version) => {
-  if (version > 0) {
-    hide(versionList);
-    show(changeBackgroundButton, backButton);
-  } else {
+/**
+ * Update elements in page showing or hiding them.
+ * @param {Boolean} reset 
+ * @returns {void}
+ */
+const updateView = (reset = false) => {
+  if (reset) {
     resetColors();
     hide(changeBackgroundButton, backButton);
     show(versionList);
+    return;
+  } else {
+    hide(versionList);
+    show(changeBackgroundButton, backButton);
+    changeBackgroundButton.dataset.
   }
 }
